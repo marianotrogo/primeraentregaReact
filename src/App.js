c39a08a88545c55f14358a58ed204d823e462268
Mariano <marianotrogo@gmail.com>
2022-12-01 20:21:09 -0300

    argegado

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Navbar from './components/Navbar/Navbar'
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
+    <div className="bg-danger">
+      <Navbar/>
+      <header className="App-header bg-secondary">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
